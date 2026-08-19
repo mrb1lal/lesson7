315091abe464536b117632981e7d53a4f555bd23
bilall571 <latifkxanovabdulvoxid@gmail.com>
2026-08-19 15:38:13 +0500

    the first push

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 export default function Banner() {
+   const { t, i18n } = useTranslation();
   return (
     <div>
-      <h1>banner</h1>
+      <h1>{t("hero.title")}</h1>
     </div>
   )
 }
